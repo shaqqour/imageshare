7a7ef547ca77b23942b80c39795e32db4e218a31
Shaqqour <mshaqqourm@gmail.com>
2020-09-18 15:46:57 -0700

    Adds two images and to the app with styling

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import logo from './assets/logo.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Image source={logo} style={{ width: 305, height: 159 }} />
+      <Image source={{ uri: "https://images.unsplash.com/photo-1457301547464-91995555cd25?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=773&q=80" }} style={{ width: 305, height: 159 }} />
       <Text style={{ color: '#888', fontSize: 18 }}>
         To share a photo from your phone with a friend, just press the button below!
       </Text>
