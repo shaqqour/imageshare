6c030df45fd55214e43dcadf3504d518370796f8
Shaqqour <mshaqqourm@gmail.com>
2020-09-18 16:40:23 -0700

    Defines async function to ask the user for permission to access the camera roll

--- App.js
+++ App.js
@@ -2,8 +2,21 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import logo from './assets/logo.png';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function App() {
+  let openImagePickerAsync = async () => {
+    let permissionResult = await ImagePicker.requestCameraRollPermissionsAsync();
+
+    if(permissionResult.granted === false) {
+      alert("Permission to access camera roll is required!");
+      return;
+    }
+
+    let pickerResult = await ImagePicker.launchImageLibraryAsync();
+    console.log(pickerResult);
+  }
+
   return (
     <View style={styles.container}>
       <Image source={logo} style={styles.logo} />
@@ -12,7 +25,7 @@ export default function App() {
         To share a photo from your phone with a friend, just press the button below!
       </Text>
       <TouchableOpacity
-      onPress={() => alert('Hello, world!')}
+      onPress={openImagePickerAsync}
       style={styles.button}>
         <Text style={styles.buttonText}>Pick a photo</Text>
       </TouchableOpacity>
